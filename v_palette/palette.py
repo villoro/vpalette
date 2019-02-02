e8060dcac1666861fe1c56f927112b1bbc149aeb
Villoro <villoro7@gmail.com>
2019-02-02 13:00:00 +0100

    Merge branch 'r/0.2.0'

--- v_palette/palette.py
+++ v_palette/palette.py
@@ -1,7 +1,7 @@
 """
     This library provides an easy way to call Material Colors from python.
 
-    Colors link: https://material.io/guidelines/style/color.html#color-color-palette
+    Available colors: https://material.io/guidelines/style/color.html#color-color-palette
 """
 
 _COLORS = {
@@ -28,21 +28,52 @@ _COLORS = {
     "white":        {50:"#FFFFFF", 100:"#FFFFFF", 200:"#FFFFFF", 300:"#FFFFFF", 400:"#FFFFFF", 500:"#FFFFFF", 600:"#FFFFFF", 700:"#FFFFFF", 800:"#FFFFFF", 900:"#FFFFFF"}  # pylint: disable=line-too-long
 }
 
-def get_colors(clist):
+_VALID_COLORS = list(_COLORS.keys())
+_VALID_INDEXS = [50] + [100*x for x in range(1, 10)]
+
+def get_one_color(name, index):
     """
-        Gives a list of colors using a list of index of the colors needed
+        Gives one color based on it's name and index.
+
+        Available colors: https://material.io/guidelines/style/color.html#color-color-palette
 
         Args:
-            clist:	list of indexs of the colors to be used
+            name:   name of the color
+            index:  intesity of the color
+    """
+    if name.lower() not in _VALID_COLORS:
+        raise ValueError(f"Color '{name}' is not valid. Valid colors are: {_VALID_COLORS}")
+
+    if index not in _VALID_INDEXS:
+        raise ValueError(f"Index {index} is not valid. Valid indexs are {_VALID_INDEXS}")
+
+    return _COLORS[name.lower()][index]
+
+
+def get_colors(data):
+    """
+        Gives hex colors.
+
+        You can pass a tuple with name and index. Example:
+            get_colors(("red", 100)) --> "#FFCDD2"
+
+        Or a list of tuples with name and index. Examples:
+            get_colors([("red", 100)]) --> ["#FFCDD2"]
+            get_colors([("red", 100), ("blue", 100)]) --> ["#FFCDD2", "#BBDEFB"]
+
+        Available colors: https://material.io/guidelines/style/color.html#color-color-palette
+
+        Args:
+            data:   tuple with (name, index) or a list of this tuples
 
         Returns:
-            list of colors in hex format
+            if input was a list --> list of hex colors
+            if input was a tuple --> hex color
     """
 
-    if isinstance(clist, list):
-        output = [_COLORS[color.lower()][index] for color, index in clist]
-    else:
-        return _COLORS[clist[0]][clist[1]]
+    # If it is a list, return a list of colors
+    if isinstance(data, list):
+        return [get_one_color(name, index) for name, index in data]
 
-    #If there is only 1 color it shouldn't return a list
-    return output if len(clist) > 1 else output[0]
+    # If it is a tuple, return one color
+    return get_one_color(data[0], data[1])
