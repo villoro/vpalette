8f5ac3b98cbbad297fb633892c0fdc85c168f12c
Villoro <villoro7@gmail.com>
2019-02-02 12:05:54 +0100

    [NEW] use a bat file to upload package to pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="v_palette",
-    version="0.1.1",
+    version="0.1.0",
     author="Arnau Villoro",
     author_email="arnau@villoro.com",
     packages=["v_palette"],
