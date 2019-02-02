d63f8306caa5c2d84f80208755e1924118831f2d
Villoro <villoro7@gmail.com>
2019-02-02 12:56:29 +0100

    [NEW] handle wrong params + test that part

--- test/test_palette.py
+++ test/test_palette.py
@@ -4,24 +4,36 @@
 
 import unittest
 
-from v_palette.palette import get_colors
+from v_palette.palette import get_colors, get_one_color
 
 
-class TestUtilities(unittest.TestCase):
-    """Test utilities"""
+class TestPalette(unittest.TestCase):
+    """Test palette"""
 
-    # ------------------------------ palette -------------------------------------------------------
-    def test_palette(self):
-        """
-            Test palette
-        """
+    def test_from_tuple(self):
+        """ Test that you can pass a tuple to get one color """
 
-        # Test that you can call one color with a list of tuples or with a tuple
-        self.assertEqual(get_colors([("red", 100)]), "#FFCDD2")
         self.assertEqual(get_colors(("red", 100)), "#FFCDD2")
 
-        # Test that you can call more than one color
+    def test_from_list_of_tuples(self):
+        """ Test that you can pass a list of tuples to get a list of colors """
+
+        self.assertEqual(
+            get_colors([("red", 100)]),
+            ["#FFCDD2"]
+        )
+
+        # Same but with two colors
         self.assertEqual(
             get_colors([("red", 100), ("blue", 100)]),
             ["#FFCDD2", "#BBDEFB"]
         )
+
+    def test_invalid_params(self):
+        """ Test that raises ValueError when input params are wrong """
+
+        # Wrong color name
+        self.assertRaises(ValueError, get_one_color, "imaginary", 100)
+
+        # Wrong color index
+        self.assertRaises(ValueError, get_one_color, "red", 77)
