945bb202712e877d6f5ef639b84427afaab0c083
Villoro <villoro7@gmail.com>
2019-03-31 21:28:56 +0200

    [TST] Test new palette

--- test/test_palette.py
+++ test/test_palette.py
@@ -18,16 +18,10 @@ class TestPalette(unittest.TestCase):
     def test_from_list_of_tuples(self):
         """ Test that you can pass a list of tuples to get a list of colors """
 
-        self.assertEqual(
-            get_colors([("red", 100)]),
-            ["#FFCDD2"]
-        )
+        self.assertEqual(get_colors([("red", 100)]), ["#FFCDD2"])
 
         # Same but with two colors
-        self.assertEqual(
-            get_colors([("red", 100), ("blue", 100)]),
-            ["#FFCDD2", "#BBDEFB"]
-        )
+        self.assertEqual(get_colors([("red", 100), ("blue", 100)]), ["#FFCDD2", "#BBDEFB"])
 
     def test_invalid_params(self):
         """ Test that raises ValueError when input params are wrong """
@@ -37,3 +31,13 @@ class TestPalette(unittest.TestCase):
 
         # Wrong color index
         self.assertRaises(ValueError, get_one_color, "red", 77)
+
+    def test_both_palettes(self):
+        """ Test that handles well other palettes """
+
+        # Request a color from default palette
+        self.assertEqual(get_colors(("orange", 100)), "#FFE0B2")
+
+        # Request from other palettes
+        self.assertEqual(get_colors(("silver", 100)), "#F2F3F4")
+        self.assertEqual(get_colors(("orange", 100), palette="flat"), "#FDEBD0")
