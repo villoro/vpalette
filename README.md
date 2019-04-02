726adb84bb1a2565beb0164ba40cac96ef68b75e
Villoro <villoro7@gmail.com>
2019-04-02 17:40:00 +0200

    Merge branch 'r/1.1.0'

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-# Palette Material Design
+# Palettes
 [![Build Status](https://travis-ci.com/villoro/v-palette.svg?branch=master)](https://travis-ci.com/villoro/v-palette)
 [![codecov](https://codecov.io/gh/villoro/v-palette/branch/master/graph/badge.svg)](https://codecov.io/gh/villoro/v-palette)
 
-Utility to easily use material design colors.
+Utility to easily use palettes
 
 ## Colors
 
-Available colors are:
+There are two palettes **material** and **flat**.
 
+### Material Colors
 <img src="https://raw.githubusercontent.com/villoro/v-palette/master/assets/material_grid.svg?sanitize=true">
 
 You can view them in a [svg file](https://github.com/villoro/v-palette/blob/master/assets/material_grid.svg).
 
 More info at [material.io](https://material.io/design/color/the-color-system.html#color-usage-palettes)
+
+### Flat Colors
+<img src="https://raw.githubusercontent.com/villoro/v-palette/master/assets/flat_grid.svg?sanitize=true">
+
+You can view them in a [svg file](https://github.com/villoro/v-palette/blob/master/assets/flat_grid.svg).
+
+More info at [html color codes](https://htmlcolorcodes.com/color-chart/flat-design-color-chart/)
 
 ## Installation
 
@@ -28,13 +36,14 @@ You can retrive one color or a list of colors using `get_colors` function:
 ```python
 from v_palette import get_colors
 
-# Retrive one color
-color = get_colors(("red", 100))
-# color will be: '#FFCDD2'
+# 1. Retrive one color
+get_colors(("red", 100)) # out: '#FFCDD2'
 
-# Retrive some colors
-colors = get_colors([("red", 100), ("blue", 100)])
-# colors will be: ['#FFCDD2', '#BBDEFB']
+# 2. Retrive some colors
+get_colors([("red", 100), ("blue", 100)]) # out: ['#FFCDD2', '#BBDEFB']
+
+# 3. Retrive colors from others palettes
+get_colors([("emerald", 100), ("silver", 100)]) # out: ['#D5F5E3', '#F2F3F4']
 ```
 
 
--- v_palette/palette.py
+++ v_palette/palette.py
@@ -1,36 +1,64 @@
 """
-    This library provides an easy way to call Material Colors from python.
+    This library provides an easy way to call colors from palettes using python.
 
-    Available colors: https://material.io/guidelines/style/color.html#color-color-palette
+    Available colors:
+        - Material: https://material.io/guidelines/style/color.html#color-color-palette
+        - Flat: https://htmlcolorcodes.com/color-chart/flat-design-color-chart/
 """
 
-from .colors import MATERIAL
+from .colors import COLORS
 
 
-_VALID_COLORS = list(MATERIAL.keys())
 _VALID_INDEXS = [50] + [100 * x for x in range(1, 10)]
 
 
-def get_one_color(name, index):
+def get_valid_colors():
+    """ Gives a list of valid colors """
+
+    valid_colors = []
+    for data in COLORS.values():
+        valid_colors += list(data.keys())
+
+    return list(set(valid_colors))
+
+
+def get_one_color(name, index, palette="material"):
     """
         Gives one color based on it's name and index.
 
-        Available colors: https://material.io/guidelines/style/color.html#color-color-palette
+        Available colors:
+        - Material: https://material.io/guidelines/style/color.html#color-color-palette
+        - Flat: https://htmlcolorcodes.com/color-chart/flat-design-color-chart/
+
+        If the color is not found it will look at the other palettes.
 
         Args:
-            name:   name of the color
-            index:  intesity of the color
+            name:       name of the color
+            index:      intesity of the color
+            palette:    name of the palette with colors ['material', 'flat']
     """
-    if name.lower() not in _VALID_COLORS:
-        raise ValueError(f"Color '{name}' is not valid. Valid colors are: {_VALID_COLORS}")
 
+    name = name.lower()
+
+    # Check index
     if index not in _VALID_INDEXS:
         raise ValueError(f"Index {index} is not valid. Valid indexs are {_VALID_INDEXS}")
 
-    return MATERIAL[name.lower()][index]
+    # Try to return it from the asked palette
+    if name in COLORS[palette]:
+        return COLORS[palette][name][index]
+
+    # Check if it is possible to retrive the color from other palettes
+    for other_palette, data in COLORS.items():
+
+        if name in COLORS[other_palette]:
+            return COLORS[other_palette][name][index]
+
+    # If color not found raise error
+    raise ValueError(f"Color '{name}' is not valid. Valid colors are: {get_valid_colors()}")
 
 
-def get_colors(data):
+def get_colors(data, palette="material"):
     """
         Gives hex colors.
 
@@ -41,10 +69,13 @@ def get_colors(data):
             get_colors([("red", 100)]) --> ["#FFCDD2"]
             get_colors([("red", 100), ("blue", 100)]) --> ["#FFCDD2", "#BBDEFB"]
 
-        Available colors: https://material.io/guidelines/style/color.html#color-color-palette
+        Available colors:
+        - Material: https://material.io/guidelines/style/color.html#color-color-palette
+        - Flat: https://htmlcolorcodes.com/color-chart/flat-design-color-chart/
 
         Args:
-            data:   tuple with (name, index) or a list of this tuples
+            data:       tuple with (name, index) or a list of this tuples
+            palette:    name of the palette with colors ['material', 'flat']
 
         Returns:
             if input was a list --> list of hex colors
@@ -53,7 +84,7 @@ def get_colors(data):
 
     # If it is a list, return a list of colors
     if isinstance(data, list):
-        return [get_one_color(name, index) for name, index in data]
+        return [get_one_color(name, index, palette) for name, index in data]
 
     # If it is a tuple, return one color
-    return get_one_color(data[0], data[1])
+    return get_one_color(data[0], data[1], palette)
