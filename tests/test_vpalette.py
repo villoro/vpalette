fbbaea4c2a08de936ab50f2cb1e5bd88c7b0479f
villoro7 <villoro7@gmail.com>
2020-05-06 19:21:19 +0200

    [NEW] Add pre-commit + first pre-commit run

--- tests/test_vpalette.py
+++ tests/test_vpalette.py
@@ -3,14 +3,16 @@ import pytest
 from vpalette import __version__
 from vpalette.palette import get_colors, get_one_color
 
+
 def test_version():
-    assert __version__ == '2.0.0'
+    assert __version__ == "2.0.0"
 
 
 def test_from_tuple():
     """ Test that you can pass a tuple to get one color """
 
     assert get_colors(("red", 100)) == "#FFCDD2"
+
 
 def test_from_list_of_tuples():
     """ Test that you can pass a list of tuples to get a list of colors """
@@ -20,16 +22,18 @@ def test_from_list_of_tuples():
     # Same but with two colors
     assert get_colors([("red", 100), ("blue", 100)]) == ["#FFCDD2", "#BBDEFB"]
 
+
 def test_invalid_params():
     """ Test that raises ValueError when input params are wrong """
 
     # Wrong color name
     with pytest.raises(ValueError):
-    	get_one_color("imaginary", 100)
+        get_one_color("imaginary", 100)
 
     # Wrong color index
     with pytest.raises(ValueError):
-    	get_one_color("red", 77)
+        get_one_color("red", 77)
+
 
 def test_both_palettes():
     """ Test that handles well other palettes """
